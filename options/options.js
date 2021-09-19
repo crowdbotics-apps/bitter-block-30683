619923d7227d50e3271fee09a0d9629f80409551
Crowdbotics <team@crowdbotics.com>
2021-09-19 16:18:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "bitter_block_30683Identifier",
+  url: "https://bitter_block_30683Identifier.botics.co",
+  api: "https://bitter_block_30683Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
